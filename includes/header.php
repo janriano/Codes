adbd42185a6bc78037ec3087772f2286d557a437
janriano <86763290+janriano@users.noreply.github.com>
2021-07-01 13:56:33 +0800

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,59 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--FontAwesome-->
+    <script src="https://kit.fontawesome.com/4321e9c1fc.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="images/logo.ico">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!--External CSS-->
+    <link rel="stylesheet" href="styles.css">
+
+    <title>BP2 ZAMPEN</title>
+</head>
+
+<body>
+
+    <main class="landing-image-home">
+    <nav class="navbar navbar-expand-lg bg-company-blue navbar-light sticky-top">
+            <a class="navbar-brand" href="#">
+                <div class="logos">
+                    <img class="logo-1" src="images/rp-coat-of-armsCMYK.png" alt="">
+                    <img class="logo-2" src="images/rdcc.png" alt="">
+                    <div class="republic">
+                        <span>Republic of the Philippines</span>
+                        <br>
+                        <span>Regional Development Council IX</span>
+                        <br>
+                        <span>Balik Probinsya Bagong Pag-asa Committee IX</span>
+                    </div>
+
+                </div>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link last-item" href="login.php">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
